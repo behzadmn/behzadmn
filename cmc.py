569a4d56f60c1894e4e016ccd3fdded37df1c65a
behzadmn <101186493+behzadmn@users.noreply.github.com>
2024-11-16 17:23:07 +0330

    Create cmc.py

---
+++ cmc.py
@@ -0,0 +1,34 @@
+headers=''
+
+session = Session()
+session.headers.update(headers)
+
+limit=400
+
+url_coin = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+# url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+parameters = {
+  'start':'1',
+  'limit':limit,
+  'convert':'USDT',
+  # 'sort':'market_cap'
+  'sort':'volume_24h'
+  # 'sort':'cmc_rank'
+}
+
+try:    
+
+
+
+        response = session.get(url_coin, params=parameters)
+        datee=datetime.now()
+        data = json.loads(response.text)['data']
+        
+        # print(data)
+        for i in range(limit):
+            
+            symbol= data[i]['symbol']
+           
+            datee=datetime.now()
+            
+            symbol=symbol+'USDT'
